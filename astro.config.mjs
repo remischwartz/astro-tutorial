4104d6bacc3f7449d193c278a49a44201bcd8bda
Rémi Schwartz <remi.schwartz@fabernovel.com>
2025-07-13 16:33:45 +0200

    chore: change github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://remischwartz.github.io",
+  site: "https://remischwartz.github.io/astro-tutorial/",
   base: "astro-tutorial",
   integrations: [preact()],
 });
