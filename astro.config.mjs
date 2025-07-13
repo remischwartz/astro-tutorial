5a750a288043bd19d9e3d429c3d857b70ea3d527
Rémi Schwartz <remi.schwartz@fabernovel.com>
2025-07-13 16:55:55 +0200

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://remischwartz.github.io/astro-tutorial/",
-  base: "astro-tutorial",
   integrations: [preact()],
 });
